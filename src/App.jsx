c0fefdc765d207dce03d2491d3855a10978a4166
Jishnu2608 <jishnudeepborah@gmail.com>
2023-09-09 21:42:39 +0530

    Loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Header from "./components/Header/Header";
 import Hero from './components/Hero/Hero';
 import './App.css';
@@ -8,31 +9,48 @@ import Contact from "./components/Contact/Contact";
 import GetStarted from './components/GetStarted/GetStarted';
 import Footer from './components/Footer/Footer';
 import Card from './components/Card/Card';
-
-
+import PropagateLoader from 'react-spinners/PropagateLoader';
 
 function App() {
+  const [Loading, SetLoading] = useState(true);
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      SetLoading(false);
+    }, 1500);
+  }, []);
+
   return (
     <div className="App">
-      <div>
-        <div className="white-gradient"/>
-        <Header/>
-        <Hero/>
-      </div>
-      <NEStates/>
-      <Explore/>  
-      <Card/>  
-      <FAQs/>
-      <Contact/>
-      <GetStarted/>
-      <Footer/>
+      {Loading ? (
+        <div className="loader" style={{"background-color": "white"}}>
+          <PropagateLoader
+            color={'#d64541'}
+            loading={true}
+            size={22}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <>
+          <div>
+            <div className="white-gradient" />
+            <Header />
+            <Hero />
+          </div>
+          <NEStates />
+          <Explore />
+          <Card />
+          <FAQs />
+          <Contact />
+          <GetStarted />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
 
 export default App;
-
-//npm run dev
-
-
-
